61bd413ef845ad9bc31f5dde2089efb16e139b4b
Vicente Manzano <46681871+DevVincent@users.noreply.github.com>
2020-06-30 12:06:05 +0100

    Update session.js

--- api/routes/session.js
+++ api/routes/session.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Session = require('../models/Session');
 
-//Fetch sessions
+//Sessions
 
 router.get('/',async(req,res)=>{
     const { id } = req.body;
